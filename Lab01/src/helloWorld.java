df2be96908989cfe18ec76001a6dccf688bc99c4
LanhhNhoo <xuanlanhbk0104@gmail.com>
2021-06-01 09:33:29 +0700

    add lab

---
+++ Lab01/src/helloWorld.java
@@ -0,0 +1,11 @@
+package lab01;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Xin chao cac ban ! \n");
+        System.out.print("Hello world ! \n");
+	}
+
+}
